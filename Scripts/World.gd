8c6e0ad4dd74cbcb596a42e3aec275ec69a2b724
Yliane Gentelet-lemainque <yliane.gentelet-lemainque@epitech.eu>
2025-05-23 19:48:27 +0200

    feat: Fishies spawn only into the AREA3D + fishies little smaller

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,1 +1,53 @@
-extends Node
+extends Node3D
+
+@onready var center_limit : Area3D = get_node("/root/Main/Center_Limit")
+@onready var edge_limit : Area3D = get_node("/root/Main/Edge_Limit")
+
+@onready var main = get_node("/root/Main/")
+
+var nb_of_fishies : int = 5000 #SET UP THE NB OF FISHES
+
+var fish_scene : PackedScene = preload("res://Scenes/Fish.tscn")
+
+var center : Vector3
+var rayon_center_limit : float
+var rayon_edge_limit : float
+
+func _ready():
+	
+	# FISH LIMIT SET UP
+	center = center_limit.global_transform.origin
+
+	var shape_center = center_limit.get_node("CollisionShape3D").shape
+
+	if shape_center is SphereShape3D:
+		rayon_center_limit = shape_center.radius
+	else:
+		return
+		
+	var shape_edge = edge_limit.get_node("CollisionShape3D").shape
+	
+	if shape_edge is SphereShape3D:
+		rayon_edge_limit = shape_edge.radius
+	else:
+		return
+	#---------------------------------------------------------------------------
+	
+	for i in range(nb_of_fishies):
+		spawn_fish()
+	
+func spawn_fish():
+	
+	var direction = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1))
+	direction = direction.normalized() #CAUSE WITHOUT FISH CAN SPWAN BEYOND THE SPHERE
+	
+	var distance_from_the_center = randf_range(rayon_center_limit, rayon_edge_limit)
+	
+	var spawn_point : Vector3 = direction * distance_from_the_center
+	
+	var fish : Node3D = fish_scene.instantiate()
+	fish.global_transform.origin = spawn_point
+
+	main.add_child(fish)
+	
+	print(spawn_point)
