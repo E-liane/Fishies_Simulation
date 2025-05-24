caf1502bb8876899b4c15c82e9006dbe85966c8c
Yliane Gentelet-lemainque <yliane.gentelet-lemainque@epitech.eu>
2025-05-24 13:49:56 +0200

    feat: Fishies now come back in area when they go out

--- Scripts/fish.gd
+++ Scripts/fish.gd
@@ -1,7 +1,18 @@
-extends StaticBody3D
+extends CharacterBody3D
 
 @export var speed : float
+
+var state : String = "in_school" # "in_school", "too_far", "too_close"
 
 func _process(delta):
 	var forward = transform.basis.x
 	position += forward * speed * delta
+	
+	if (state == "too_far"):
+		position = position - position.normalized() / 10
+	elif (state == "too_close"):
+		position = position + position.normalized() / 10
+
+func change_state(new_state : String):
+	state = new_state
+	print(state)
